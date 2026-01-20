07008be9809c26860060ffd11151d25b6e97a2a6
Rohith007-cys <00rohith00Sankar00@gmail.com>
2026-01-20 12:04:14 +0530

    Delete Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++
@@ -1,4 +0,0 @@
-weight = float(input("Enter package weight in kg: "))
-rate = float(input("Enter shipping rate per kg: "))
-shipping_cost = weight * rate
-print(f"Shipping Cost: {shipping_cost} USD")
